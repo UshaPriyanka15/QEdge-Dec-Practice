1583374af8129658d11898b4288f9b90ef0fc205
UshaPriyanka15 <ushapriya8015@gmail.com>
2024-12-21 17:15:00 +0530

    added java methods code

---
+++ reports.java
@@ -0,0 +1,12 @@
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}[A[A[A[A[A[A[A[A[D
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+[B[B[B[B[B[B[B[C
